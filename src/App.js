44c5bf7d299a1689f4ee49604afc34e95dd7ff05
iiyass <ilyass1.ib@outlook.com>
2020-12-16 03:44:09 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -1,34 +1,33 @@
-import React, { Suspense } from 'react';
+import React from 'react';
+import Group from './Container/ChatGroup'
+import Contact from './Container/Contact'
+import Chat from './Container/TeamChat'
+import History from './Container/History'
+import Profil from './Components/Profil'
+import Signup from './Components/Signup'
+import Login from './Components/Login'
+import Video from '../src/Components/WebChat'
+import PrivateRoute from './hooks/PrivateRoute'
+import LandingRoute from './hooks/LandingRoute'
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import Landing from './Components/Landing';
 
-const Landing = React.lazy(() => import('./Components/Landing'));
-const Contact = React.lazy(() => import('./Container/Contact'));
-const Group = React.lazy(() => import('./Container/ChatGroup'));
-const Chat = React.lazy(() => import('./Container/TeamChat'));
-const History = React.lazy(() => import('./Container/History'));
-const Profil = React.lazy(() => import('./Components/Profil'));
-const Signup = React.lazy(() => import('./Components/Signup'));
-const Login = React.lazy(() => import('./Components/Login'));
-const Video = React.lazy(() => import('../src/Components/WebChat'));
-const PrivateRoute = React.lazy(() => import('./hooks/PrivateRoute'));
-const LandingRoute = React.lazy(() => import('./hooks/LandingRoute'));
 
 const App = () => {
   const token = localStorage.getItem('token');
   return (
     <BrowserRouter>
       <Switch>
-        <Suspense fallback={<div>Chargement...</div>}>
-          <LandingRoute path="/" exact component={Landing} />
-          <PrivateRoute path="/history" exact component={History} />
-          <PrivateRoute path="/chat" exact component={Chat} />
-          <PrivateRoute path="/contact" exact component={Contact} />
-          <PrivateRoute path="/alert" exact component={Video} />
-          <PrivateRoute path="/groups" exact component={Group} />
-          <PrivateRoute path="/profil" exact component={Profil} />
-          <Route path="/signup" exact render={props => token ? <Redirect to='/contact' /> : <Signup {...props} />} />
-          <Route path="/login" exact render={props => token ? <Redirect to='/contact' /> : <Login {...props} />} />
-        </Suspense>
+        <LandingRoute path="/" exact component={Landing} />
+        <PrivateRoute path="/history" exact component={History} />
+        <PrivateRoute path="/chat" exact component={Chat} />
+        <PrivateRoute path="/contact" exact component={Contact} />
+        <PrivateRoute path="/alert" exact component={Video} />
+        <PrivateRoute path="/groups" exact component={Group} />
+        <PrivateRoute path="/profil" exact component={Profil} />
+        <Route path="/signup" exact render={props => token ? <Redirect to='/contact' /> : <Signup {...props} />} />
+        <Route path="/login" exact render={props => token ? <Redirect to='/contact' /> : <Login {...props} />} />
+
       </Switch>
     </BrowserRouter >
 
